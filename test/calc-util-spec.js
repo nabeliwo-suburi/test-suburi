185b2d34209bafa0a2d555d1f4d06b89b95dcc0d
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-05-09 23:12:53 +0900

    web packを導入

---
+++ test/calc-util-spec.js
@@ -0,0 +1,17 @@
+var calcUtil = require('../src/calc-util.js');
+
+describe('add関数のテスト', function() {
+  it('1+2は3である', function() {
+    if (calcUtil.add(1, 2) === 3) {
+    } else {
+        throw new Error('失敗');
+    }
+  });
+
+  it('1+2は4である', function() {
+    if (calcUtil.add(1, 2) === 4) {
+    } else {
+        throw new Error('失敗');
+    }
+  });
+});
